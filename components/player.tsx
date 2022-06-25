7ed2c3445ded9b7bdc97b6657b99ae324267d9e8
Ruiqi Huang <kiyoakila@gmail.com>
2022-06-25 15:33:38 -0700

    player bar layout

---
+++ components/player.tsx
@@ -0,0 +1,236 @@
+import {
+  ButtonGroup,
+  Box,
+  IconButton,
+  RangeSlider,
+  RangeSliderFilledTrack,
+  RangeSliderTrack,
+  RangeSliderThumb,
+  Center,
+  Flex,
+  Text,
+} from "@chakra-ui/react";
+import ReactHowler from "react-howler";
+import { useEffect, useRef, useState } from "react";
+import {
+  MdShuffle,
+  MdSkipPrevious,
+  MdSkipNext,
+  MdOutlinePlayCircleFilled,
+  MdOutlinePauseCircleFilled,
+  MdOutlineRepeat,
+} from "react-icons/md";
+import { useStoreActions } from "easy-peasy";
+import { formatTime } from "../lib/formatters";
+
+const Player = ({ songs, activeSong }) => {
+  // a song is playing?
+  const [playing, setPlaying] = useState(true);
+  // which song is playing?
+  const [index, setIndex] = useState(
+    songs.findIndex((s) => s.id === activeSong.id)
+  );
+  // value of the seek
+  const [seek, setSeek] = useState(0.0);
+  // user is grabbing the thumb or not
+  const [isSeeking, setIsSeeking] = useState(false);
+  // repeat mode
+  const [repeat, setRepeat] = useState(false);
+  // shuffle mode
+  const [shuffle, setShuffle] = useState(false);
+  // duration of the song
+  const [duration, setDuration] = useState(0.0);
+  const soundRef = useRef(null);
+  const repeatRef = useRef(repeat);
+  const setActiveSong = useStoreActions((state: any) => state.changeActiveSong);
+
+  useEffect(() => {
+    let timerId;
+
+    if (playing && !isSeeking) {
+      const f = () => {
+        setSeek(soundRef.current.seek());
+        timerId = requestAnimationFrame(f);
+      };
+
+      timerId = requestAnimationFrame(f);
+      return () => cancelAnimationFrame(timerId);
+    }
+
+    cancelAnimationFrame(timerId);
+  }, [playing, isSeeking]);
+
+  useEffect(() => {
+    setActiveSong(songs[index]);
+  }, [index, setActiveSong, songs]);
+
+  useEffect(() => {
+    repeatRef.current = repeat;
+  }, [repeat]);
+
+  const setPlayState = (value) => {
+    setPlaying(value);
+  };
+
+  const onShuffle = () => {
+    setShuffle((state) => !state);
+  };
+
+  const onRepeat = () => {
+    setRepeat((state) => !state);
+  };
+
+  const prevSong = () => {
+    setIndex((state) => {
+      return state ? state - 1 : songs.length - 1;
+    });
+  };
+
+  const nextSong = () => {
+    setIndex((state) => {
+      if (shuffle) {
+        const next = Math.floor(Math.random() * songs.length);
+
+        if (next === state) {
+          return nextSong();
+        }
+        return next;
+      }
+
+      return state === songs.length - 1 ? 0 : state + 1;
+    });
+  };
+
+  const onEnd = () => {
+    if (repeatRef.current) {
+      setSeek(0);
+      soundRef.current.seek(0);
+    } else {
+      nextSong();
+    }
+  };
+
+  const onLoad = () => {
+    const songDuration = soundRef.current.duration();
+    setDuration(songDuration);
+  };
+
+  const onSeek = (e) => {
+    setSeek(parseFloat(e[0]));
+    soundRef.current.seek(e[0]);
+  };
+
+  return (
+    <Box>
+      <Box>
+        <ReactHowler
+          playing={playing}
+          src={activeSong?.url}
+          ref={soundRef}
+          onLoad={onLoad}
+          onEnd={onEnd}
+        />
+      </Box>
+      {/* buttons */}
+      <Center color="gray.600">
+        <ButtonGroup>
+          {/* shuffle */}
+          <IconButton
+            outline="none"
+            variant="link"
+            aria-label="shuffle"
+            fontSize="24px"
+            color={shuffle ? "white" : "gray.600"}
+            onClick={onShuffle}
+            icon={<MdShuffle />}
+          />
+          {/* previous */}
+          <IconButton
+            outline="none"
+            variant="link"
+            aria-label="skip"
+            fontSize="24px"
+            icon={<MdSkipPrevious />}
+            onClick={prevSong}
+          />
+          {playing ? (
+            // pause
+            <IconButton
+              outline="none"
+              variant="link"
+              aria-label="pause"
+              fontSize="40px"
+              color="white"
+              icon={<MdOutlinePauseCircleFilled />}
+              onClick={() => setPlayState(false)}
+            />
+          ) : (
+            // play
+            <IconButton
+              outline="none"
+              variant="link"
+              aria-label="play"
+              fontSize="40px"
+              color="white"
+              icon={<MdOutlinePlayCircleFilled />}
+              onClick={() => setPlayState(true)}
+            />
+          )}
+          {/* next */}
+          <IconButton
+            outline="none"
+            variant="link"
+            aria-label="next"
+            fontSize="24px"
+            icon={<MdSkipNext />}
+            onClick={nextSong}
+          />
+          {/* repeat */}
+          <IconButton
+            outline="none"
+            variant="link"
+            aria-label="repeat"
+            fontSize="24px"
+            color={repeat ? "white" : "gray.600"}
+            onClick={onRepeat}
+            icon={<MdOutlineRepeat />}
+          />
+        </ButtonGroup>
+      </Center>
+
+      <Box color="gray.600">
+        <Flex justify="center" align="center">
+          {/* current run time */}
+          <Box width="10%">
+            <Text fontSize="xs">{formatTime(seek)}</Text>
+          </Box>
+          {/* seek bar */}
+          <Box width="80%">
+            <RangeSlider
+              aria-label={["min", "max"]}
+              step={0.1}
+              min={0}
+              id="player-range"
+              max={duration ? duration.toFixed(2) : 0}
+              onChange={onSeek}
+              value={[seek]}
+              onChangeStart={() => setIsSeeking(true)}
+              onChangeEnd={() => setIsSeeking(false)}
+            >
+              <RangeSliderTrack bg="gray.800">
+                <RangeSliderFilledTrack bg="gray.600" />
+              </RangeSliderTrack>
+              <RangeSliderThumb index={0} />
+            </RangeSlider>
+          </Box>
+          {/* song duration */}
+          <Box width="10%" textAlign="right">
+            <Text fontSize="xs">{formatTime(duration)}</Text>
+          </Box>
+        </Flex>
+      </Box>
+    </Box>
+  );
+};
+
+export default Player;
---
+++ components/songTable.tsx
@@ -0,0 +1,63 @@
+import { Box } from "@chakra-ui/layout";
+import { Table, Thead, Td, Tr, Tbody, Th, IconButton } from "@chakra-ui/react";
+import { BsFillPlayFill } from "react-icons/bs";
+import { AiOutlineClockCircle } from "react-icons/ai";
+import { useStoreActions } from "easy-peasy";
+import { formatDate, formatTime } from "../lib/formatters";
+
+const SongTable = ({ songs }) => {
+  return (
+    <Box bg="transparent" color="white">
+      {/* container  */}
+      <Box padding="10px" marginBottom="20px">
+        {/* play icon */}
+        <Box marginBottom="30px">
+          <IconButton
+            icon={<BsFillPlayFill fontSize="30px" />}
+            aria-label="play"
+            colorScheme="green"
+            size="lg"
+            isRound
+          />
+        </Box>
+        {/* song table */}
+        <Table variant="unstyled">
+          {/* table head */}
+          <Thead borderBottom="1px solid" borderColor="rgba(255,255,255,0.2)">
+            <Tr>
+              <Th>#</Th>
+              <Th>Title</Th>
+              <Th>Date Added</Th>
+              <Th>
+                <AiOutlineClockCircle />
+              </Th>
+            </Tr>
+          </Thead>
+          {/* table body */}
+          <Tbody>
+            {songs.map((song, i) => (
+              <Tr
+                // highlight when hover over
+                sx={{
+                  transition: "all .3s ",
+                  "&:hover": {
+                    bg: "rgba(255,255,255, 0.1)",
+                  },
+                }}
+                key={song.id}
+                cursor="cursor"
+              >
+                <Td>{i + 1}</Td>
+                <Td>{song.name}</Td>
+                <Td>{formatDate(song.createdAt)}</Td>
+                <Td>{formatTime(song.duration)}</Td>
+              </Tr>
+            ))}
+          </Tbody>
+        </Table>
+      </Box>
+    </Box>
+  );
+};
+
+export default SongTable;
